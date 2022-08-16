2b8978b07064901e166d1a2a170ea108492a26bb
Justin <junitsu00@gmail.com>
2022-08-16 09:21:55 -0500

    slides in done

--- app/routes.py
+++ app/routes.py
@@ -4,8 +4,13 @@ from flask import render_template
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    movies = ['Oldboy','The Matrix','Big Trouble in Little China', 'Ip Man', 'The Other Guys']
+    return render_template('index.html', movies=movies)
+
+@app.route('/home')
+def home():
+    return 'Welcome Home'
 
 @app.route('/test')
 def test():
-    return 'This is a test'
+    return render_template('test.html')
